6fa01ccab1811108abc9e3dbe26090a64a03d5f2
krissidibe <assowlove@gmail.com>
2024-01-10 02:38:09 +0000

    07 Janv  bugs236

---
+++ src/app/customer/_components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import { LayoutDashboard } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='bg-zinc-300/10 p-4 border-r min-w-[250px] w-[250px] h-full'>
+
+<Image className='p-2' src={"/images/logo.png"} width={100} height={100} alt='' />
+
+<div className='h-full flex flex-col gap-3 pt-10'>
+
+    <p className='flex gap-2 p-2 w-full  hover:bg-orange-400 hover:text-white cursor-pointer rounded-md'> <LayoutDashboard/> Dashboard</p>
+    <p className='flex gap-2 p-2 w-full  hover:bg-orange-400 hover:text-white cursor-pointer rounded-md'> <LayoutDashboard/> Dashboard</p>
+    <p className='flex gap-2 p-2 w-full  hover:bg-orange-400 hover:text-white cursor-pointer rounded-md'> <LayoutDashboard/> Dashboard</p>
+    <p className='flex gap-2 p-2 w-full  hover:bg-orange-400 hover:text-white cursor-pointer rounded-md'> <LayoutDashboard/> Dashboard</p>
+    <p className='flex gap-2 p-2 w-full  hover:bg-orange-400 hover:text-white cursor-pointer rounded-md'> <LayoutDashboard/> Dashboard</p>
+
+</div>
+    </div>
+  )
+}
+
+export default Sidebar
