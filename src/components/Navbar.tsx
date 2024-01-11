f3e42ebe6ca171191d8f2ce6c0bd7f930f2e5dae
krissidibe <assowlove@gmail.com>
2024-01-11 12:33:51 +0000

    11 janv7

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,17 @@ import { Input } from './ui/input'
 import { ShoppingBasket, UserIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Navbar() {
   return (
     <div className='bg-white border-b border-black w-full fixed z-50 flex flex-row  items-center justify-center'>
         <div className='h-[70px] flex   z-50  px-10 xl:px-0 max-w-7xl w-full   justify-between gap-4 items-center '>
           <div>
+          <Link href={"/"}>
+                
             <Image src={"/images/logo.png"} width={100} height={100} alt='' />
+              </Link>
           </div>
             
            <div className='flex-1 flex gap-4 max-w-xl'>
@@ -19,12 +23,14 @@ function Navbar() {
 <Button>Recherche</Button>
            </div>
             <div className=' flex gap-4'>
+              <Link href={"/customer"}>
                 <p className='flex gap-1 justify-center items-center'>
                 <UserIcon/>
                     Se connecter</p>
-                <p className='flex gap-1 justify-center items-center'>
+              </Link>
+              {/*   <p className='flex gap-1 justify-center items-center'>
                 <ShoppingBasket/>
-                    Panier</p>
+                    Panier</p> */}
             </div>
             
         </div>
